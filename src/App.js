c026a21bcb879941c2ae234c5f22203c005fdb69
John <john.senekal@onedayonly.co.za>
2021-03-15 10:24:13 +0200

    main screen.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import MediaCard from './Components/MediaCard';
+import Grid from '@material-ui/core/Grid';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [developers, setDevelopers] = useState([]);
+  const [staleData, setStaleData] = useState(false);
+
+  function handleChange(newValue) {
+    setStaleData(newValue);
+  }
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:3001/developers')
+        .then(res => res.json())
+        .then(data => {
+          setDevelopers(data)
+          console.log("data receieved: ", data)
+        })
+        .catch(console.log);
+  }, [staleData])
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>PROJECT MONITORING DASHBOARD</h2>
+      <Grid
+                container
+                spacing={2}
+                direction="row"
+                justify="flex-start"
+                alignItems="flex-start"
+            >
+              {developers.map( developer =>
+                 <MediaCard 
+                    url={developer.image} 
+                    name={developer.name}
+                    project={developer.project}
+                    info={developer.info}
+                    score={developer.score}
+                    staleData={staleData}
+                    onChange={handleChange}
+                  />
+              )}
+        </Grid>
       </header>
     </div>
   );
